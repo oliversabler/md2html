c60b877bfc4f8a1fd0e8550cae16d1bba58ee453
oliversabler <oliver.sabler@gmail.com>
2023-07-14 19:12:54 +0200

    Add 2 integration tests, more coming soon™

---
+++ tests/cli.rs
@@ -0,0 +1,27 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn required_arg_missing() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("md2html")?;
+
+    cmd.arg("");
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "error: unexpected argument \'\' found",
+    ));
+
+    Ok(())
+}
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("md2html")?;
+
+    cmd.arg("--path").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Failed to read the markdown file"));
+
+    Ok(())
+}
